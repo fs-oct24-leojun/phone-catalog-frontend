4fb8c1e055bc0a647b66b340a42a10a00f9deb5c
Liliia Borodina <110099141+LiliiaVol@users.noreply.github.com>
2025-01-10 11:51:37 +0200

    Created card item component (#2)
    * Created card item component
    * Added variables to CardItem.scss and created new variables at variables.scss

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,6 +5,7 @@ import {
   } from 'react-router-dom';
   import { App } from './App';
   import { NotFoundPage } from './components/pages/NotFoundPage/NotFoundPage';
+  import React from 'react';
   
   // TODO: Add more pages to the routing.
   // Remove 'index' from NotFoundPage and replace it with '*'
---
+++ src/components/CardItem/CardItem.tsx
@@ -0,0 +1,41 @@
+import './CardItem.scss';
+import phoneImage from '/img/phones/apple-iphone-11-pro-max/gold/00.webp';
+import React from 'react';
+
+export const CardItem: React.FC = () => {
+  return (
+    <div className="cart-item">
+      <div className="cart-item__container container">
+        <button className="cart-item__close-button">
+          <i className="fa-solid fa-xmark" style={{ color: '#b4bdc3' }} />
+        </button>
+
+        <div className="cart-item__phone-image">
+          <img
+            src={phoneImage}
+            alt="Apple iPhone 11 Pro Max 64GB Gold"
+            className="cart-item__phone-img"
+          />
+        </div>
+
+        <p className="cart-item__title">
+          Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)
+        </p>
+      </div>
+
+      <div className="cart-item__container container">
+        <div className="cart-item__counter">
+          <button className="cart-item__button button--round">
+            <i className="fa-solid fa-minus" style={{ color: '#b4bdc3' }} />
+          </button>
+          <p className="cart-item__count-text">1</p>
+          <button className="cart-item__button button--round">
+            <i className="fa-solid fa-plus is-active" style={{ color: '#0f0f11' }} />
+          </button>
+        </div>
+
+        <p className="cart-item__price">$1099</p>
+      </div>
+    </div>
+  );
+}

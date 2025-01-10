2bf362e617f7fabe289eca9c16c077814a6b8f9e
Костянтин <91181748+YoNiga7@users.noreply.github.com>
2025-01-10 12:33:47 +0200

    add product card (#3)
    * add product card
    
    * product card minor fix

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,66 @@
+
+import React, { useState } from "react";
+import './ProductCard.scss'
+import { shownProductCardCharacteristics } from "../../constants/constants";
+import { Product } from "../../types/Product";
+
+interface Props {
+  product: Product;
+}
+
+export const ProductCard: React.FC<Props> = ({
+  product,
+}) => {
+  const { id, category, name, price, fullPrice, image } = product;
+  
+  const [isInCart, setIsInCart] = useState(false);
+  const [isInFavourite, setIsInFavourite] = useState(false);
+
+
+  return (
+    <article className="product-card">
+      <img
+        src={image}
+        alt={`${category}_image`}
+        className="product-card__image"
+      />
+
+      <p className="product-card__title">{name}</p>
+
+      <div className="product-card__price">
+        <p className="product-card__actual-price h3">{`$${price}`}</p>
+        {fullPrice !== price && (
+          <p className="product-card__full-price h3">{`$${fullPrice}`}</p>
+        )}
+      </div>
+
+      <div className="product-card__characteristics">
+        {shownProductCardCharacteristics.map((characteristic) => (
+          <p className="product-card__characteristic" key={id}>
+            <span className="product-card__characteristic-name small-text">
+              {characteristic}
+            </span>
+            <span className="product-card__characteristic-value small-text">
+              {product[characteristic.toLowerCase() as keyof Product]}
+            </span>
+          </p>
+        ))}
+      </div>
+
+      <div className="product-card__buttons">
+        <button
+          onClick={() => setIsInCart((prev) => !prev)}
+          type="button"
+          className={`button button-add ${isInCart && 'button-add--selected'}`}>
+          {isInCart ? 'Added' : 'Add to cart'}
+        </button>
+
+        <button
+          onClick={() => setIsInFavourite((prev) => !prev)}
+          type="button"
+          className={`button button-favourite ${isInFavourite && 'button-favourite--selected'}`}
+        />
+      </div>
+    </article>
+  );
+};
---
+++ src/constants/constants.ts
@@ -0,0 +1,2 @@
+export const shownProductCardCharacteristics = ['Screen', 'Capacity', 'RAM'];
+export const tabletWidth = 640;

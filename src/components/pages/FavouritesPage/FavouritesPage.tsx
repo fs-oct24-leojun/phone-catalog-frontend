5c7ff8e052e1d824b7bc4fbf0c04997cd525a606
Liliia Borodina <110099141+LiliiaVol@users.noreply.github.com>
2025-01-16 11:03:50 +0200

    Added FavouritesPage and set up local storage (#16)

--- src/components/pages/FavouritesPage/FavouritesPage.tsx
+++ src/components/pages/FavouritesPage/FavouritesPage.tsx
@@ -1,1 +1,48 @@
-export const FavouritesPage: React.FC = () => <h1>That is favourites page</h1>;
+import './FavouritesPage.scss';
+import React, { useEffect, useState } from 'react';
+import { ProductCard } from '../../ProductCard/ProductCard.tsx';
+import { Link } from 'react-router-dom';
+import { Product } from '../../../types/Product.ts';
+
+export const FavouritesPage: React.FC = () => {
+  const [favourites, setFavourites] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const favourites = JSON.parse(localStorage.getItem('favourites') || '[]');
+
+    setFavourites(favourites);
+  }, [favourites]);
+
+  return (
+    <div className="favourites-page">
+      <section className="routing-section">
+        <Link to="/">
+          <i
+            className="fa-solid fa-house"
+            style={{ color: '#0F0F11' }}
+          />
+        </Link>
+        <i className="button__icon fa-solid fa-angle-right" />
+        <a className="routing-section__text">Favourites</a>
+      </section>
+      <section className="hero-section">
+        <h1 className="hero-section__title headline--1">Favourites</h1>
+        <p className="hero-section__text">{favourites.length} items</p>
+      </section>
+      <section className="items-section">
+        <div className="items-section__container container">
+          {favourites.length ?
+            favourites.map((product: Product) => (
+              <div
+                className="items-section__item"
+                key={product.id}
+              >
+                <ProductCard product={product} />
+              </div>
+            ))
+            : <h1>Favourites is empty</h1>}
+        </div>
+      </section>
+    </div>
+  );
+};

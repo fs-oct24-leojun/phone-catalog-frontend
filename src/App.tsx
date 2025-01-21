983720dfeff38ea432a261e38ebe5bc69c85df87
AnderZerfall <anya.androschuk@gmail.com>
2025-01-22 01:31:49 +0200

    Implemented Firebase test (ft.Ruslan) (#40)
    * Merged the most actual branches together + code refactoring + loader fix + search fix
    
    * style fix
    
    * hot fix for Netlify
    
    * Rename SearchHelper.ts to searchHelper.ts
    
    * price fix
    
    * Update SearchBar.tsx
    
    * dark theme implementation
    
    * dark theme implemented
    
    * hot-fix-theme-change
    
    * firebase-test-implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,36 @@ import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { NotificationProvider } from './context/NotificationContext';
 import { ThemeContext } from './context/ThemeContext';
+import { AuthProvider } from './context/AuthContext';
 import { useContext } from 'react';
 
 export const App: React.FC = () => {
   const { theme } = useContext(ThemeContext);
 
-  console.log(`App: ${theme}`);
+  // TO DO: Implement Firebase
+  // const [user, setUser] = useState(null);
+
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+  //     setUser(currentUser);
+  //   });
+
+  //   return () => unsubscribe();
+  // }, []);
 
   return (
-    <NotificationProvider>
-      <div className="theme-wrapper" data-theme={`${theme}`}>
-        <Header />
-        <main className="wrapper">
-          <div className="wrapper__container container">
-            <Outlet />
-          </div>
-        </main>
-        <Footer />
-      </div>
-    </NotificationProvider>
+    <AuthProvider>
+      <NotificationProvider>
+        <div className="theme-wrapper" data-theme={`${theme}`}>
+          <Header />
+          <main className="wrapper">
+            <div className="wrapper__container container">
+              <Outlet />
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </NotificationProvider>
+    </AuthProvider>
   );
 };
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,59 @@
+import {onAuthStateChanged, User} from 'firebase/auth';
+import { auth } from '../firebases/firebase';
+
+import React, {
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+type CurrentUser = User | null
+
+export const AuthContext = React.createContext({
+  currentUser: {} as CurrentUser, 
+  userLoggedIn: false,
+  loading: true  
+});
+
+export const useAuth = () => useContext(AuthContext);
+
+type Props = {
+    children: React.ReactNode
+}
+
+
+export const AuthProvider: React.FC<Props> = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<CurrentUser>(null);
+  const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+
+  const initializeUser = async (user: CurrentUser) => {
+    if (user) {
+      setCurrentUser({...user});
+      setUserLoggedIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLoggedIn(false);
+    }
+
+    setLoading(false);
+  }
+
+  const value = {
+    currentUser, userLoggedIn, loading
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, initializeUser);
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value = {value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+
+}
